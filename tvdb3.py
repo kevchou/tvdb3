0e74e9c787884259a7cc1129e025e8d0deeda48e
Kevin Chou <kevchou@gmail.com>
2016-02-27 01:10:03 -0500

    changed how show inits work

--- tvdb3.py
+++ tvdb3.py
@@ -20,24 +20,47 @@ exts = ('.mkv', '.mp4', '.avi')
 
 # REGULAR EXPRESSIONS FOR FILE NAME SEARCHES
 # Multi episode files, like 'S09E01 - E02"
-multi_ep_regex = re.compile('([sS][0-9]+[eE][0-9]+.*[eE][0-9]+)|([0-9]+(x|\.)[0-9]+(x|\.)[0-9]+)')
+# multi_ep_regex = re.compile('([sS][0-9]+[eE][0-9]+.*[eE][0-9]+)|([0-9]+(x|\.)[0-9]+(x|\.)[0-9]+)')
 
-# Looks for strings like "S09E01" or "9x01", "9.01"
-single_ep_regex = re.compile('([sS][0-9]+.?[eE][0-9]+)|([0-9]+(x|\.)[0-9]+)')
+# # Looks for strings like "S09E01" or "9x01", "9.01"
+# single_ep_regex = re.compile('([sS][0-9]+.?[eE][0-9]+)|([0-9]+(x|\.)[0-9]+)')
 
-# Regex for file extensions
-video_ext_regex = re.compile('(\.mkv|\.avi|\.mp4)')
+# # Regex for file extensions
+# video_ext_regex = re.compile('(\.mkv|\.avi|\.mp4)')
 
-s_rgx = re.compile('[sS][0-9]+')
-e_rgx = re.compile('[eE][0-9]+')
-alt_rgx = re.compile('(x|\.)')
+# s_rgx = re.compile('[sS][0-9]+')
+# e_rgx = re.compile('[eE][0-9]+')
+# alt_rgx = re.compile('(x|\.)')
+
 
 
 class Show(dict):
-    def __init__(self, show_name):
-        dict.__init__(self)
-        self.show_name = show_name
+    """ A Show object that holds all the episodes of a show """
 
+    def __init__(self, series_id):
+        dict.__init__(self)
+
+        # Query tvdb and return a BeautifulSoup object
+        soup = self.get_tvdb_soup(series_id)
+
+        self.show_name = soup.find('Series').find('SeriesName').text
+
+        for ep in soup.find_all('Episode'):
+            
+            season_num = int(ep.find("SeasonNumber").text)
+            ep_num     = int(ep.find("EpisodeNumber").text)
+            ep_name    = ep.find("EpisodeName").text
+            air_date   = ep.find("FirstAired").text
+
+            if season_num not in self:
+                self[season_num] = Season(show_name = self.show_name,
+                                          season_num = season_num)
+
+            self[season_num][ep_num] = Episode(episode_title=ep_name,
+                                               season_num=season_num,
+                                               episode_num=ep_num,
+                                               air_date=air_date)
+            
     def __getitem__(self, season_num):
         if season_num in self:
             return dict.__getitem__(self, season_num)
@@ -46,18 +69,39 @@ class Show(dict):
                                                       season_num=season_num))
             return dict.__getitem__(self, season_num)
 
-    def __str__(self):
-        print("{name:s} - {num_seas:d} Seasons".format(name=self.show_name,
-                                                       num_seas=len(self)))
+    def __repr__(self):
+        return "{name:s} - {num_seas:d} Seasons".format(name=self.show_name,
+                                                        num_seas=len(self))
 
-    def get_next_air(self):
-        all_eps = []
+    def print_next_air(self):
+        """ Finds unaired episodes and prints them """
+        not_aired = []
 
         for season in self.values():
-            print(season)
+            for episode in season.values():
+                if datetime.strptime(episode.air_date, '%Y-%m-%d') > datetime.now():
+                    not_aired.append(episode)
+
+        for ep in not_aired:
+            print('{a} {n}'.format(a=ep.air_date, n=ep))
+
+            
+    def get_tvdb_soup(self, series_id):
+        """ Reads xml file from tvdb and turns it into a BeautifulSoup object """
+        
+        url = TVDB_URL + APIKEY + SERIES_EPISODES_URL.format(id=series_id)
+        request = urllib.request.urlopen(url)
+
+        if request.code == 200:
+            
+            raw_xml = request.read()
+            soup = BeautifulSoup(raw_xml, 'xml')
+            
+            return soup
         
 
 class Season(dict):
+    
     def __init__(self, show_name, season_num):
         dict.__init__(self)
         self.show_name = show_name
@@ -73,14 +117,16 @@ class Season(dict):
             return dict.__getitem__(self, episode_num)
 
     def __str__(self):
-        print("List of episodes for season {:d}:\n".format(self.season_num) +
+        return("Season {:d}:\n".format(self.season_num) +
               "\n".join(self.get_episode_list()))
 
     def get_episode_list(self):
-        return [str(ep.ep) + " - " + ep.title for ep in self.values()]
+        return ["{num} - {title}".format(num=ep.ep_num, title=ep.episode_title)
+                for ep in self.values()]
 
-
+                
 class Episode:
+    
     def __init__(self, episode_title, season_num, episode_num, air_date=None):
         self.episode_title = episode_title
         self.season_num = season_num
@@ -88,9 +134,9 @@ class Episode:
         self.air_date = air_date
 
     def __str__(self):
-        print("S{season:02d}E{ep:02d} - {title:s}".format(season=self.season_num,
-                                                          ep=self.ep_num,
-                                                          title=self.episode_title))
+        return "S{season:02d}E{ep:02d} - {title:s}".format(season=self.season_num,
+                                                           ep=self.ep_num,
+                                                           title=self.episode_title)
 
 
 def search(query):
@@ -125,57 +171,62 @@ def search(query):
     return results_array
 
 
-def get_show_info(series_id):
-    url = TVDB_URL + APIKEY + SERIES_INFO_URL.format(id=series_id)
+# def get_show_info(series_id):
+#     url = TVDB_URL + APIKEY + SERIES_INFO_URL.format(id=series_id)
 
-    request = urllib.request.urlopen(url)
+#     request = urllib.request.urlopen(url)
 
-    if request.code == 200:
-        raw_xml = request.read()
-        soup = BeautifulSoup(raw_xml, 'xml')
+#     if request.code == 200:
+#         raw_xml = request.read()
+#         soup = BeautifulSoup(raw_xml, 'xml')
 
-        show = soup.find('Series')
+#         show = soup.find('Series')
 
-        show_name = show.find('SeriesName').text
-        show_id = show.find('id').text
+#         show_name = show.find('SeriesName').text
+#         show_id = show.find('id').text
 
-        print('{id} - {name}'.format(id=show_id, name=show_name))
-
-
-def get_show_episodes(series_id):
-    url = TVDB_URL + APIKEY + SERIES_EPISODES_URL.format(id=series_id)
-    request = urllib.request.urlopen(url)
-
-    if request.code == 200:
-
-        raw_xml = request.read()
-        soup = BeautifulSoup(raw_xml, 'xml')
-
-        series = soup.find('Series')
-        show_name = series.find('SeriesName').text
-
-        show = Show(show_name)
-
-        for ep in soup.find_all('Episode'):
-            season_num = ep.find("SeasonNumber").text
-            ep_num = ep.find("EpisodeNumber").text
-            ep_name = ep.find("EpisodeName").text
-            air_date = ep.find("FirstAired").text
-
-            season_num = int(season_num)
-            ep_num = int(ep_num)
-            air_date = datetime.strptime(air_date, '%Y-%m-%d')
-
-            show[season_num][ep_num] = Episode(ep_name,
-                                               season_num,
-                                               ep_num,
-                                               air_date)
-        return show
+#         print('{id} - {name}'.format(id=show_id, name=show_name))
 
 
-b = get_show_episodes(275557)
+# def get_show_episodes(series_id):
+#     url = TVDB_URL + APIKEY + SERIES_EPISODES_URL.format(id=series_id)
+#     request = urllib.request.urlopen(url)
 
-print([list(s.values()) for s in b.values()])
+#     if request.code == 200:
+
+#         raw_xml = request.read()
+#         soup = BeautifulSoup(raw_xml, 'xml')
+
+#         series = soup.find('Series')
+#         show_name = series.find('SeriesName').text
+
+#         show = Show(show_name)
+
+#         for ep in soup.find_all('Episode'):
+#             season_num = ep.find("SeasonNumber").text
+#             ep_num = ep.find("EpisodeNumber").text
+#             ep_name = ep.find("EpisodeName").text
+#             air_date = ep.find("FirstAired").text
+
+#             season_num = int(season_num)
+#             ep_num = int(ep_num)
+#             air_date = datetime.strptime(air_date, '%Y-%m-%d')
+
+#             show[season_num][ep_num] = Episode(ep_name,
+#                                                season_num,
+#                                                ep_num,
+#                                                air_date)
+#         return show
+
+
+b = 275557
+s = 79169
+
+
         
-print([ep for ep in b[3].values() if ep.air_date > datetime.now()])
+# print([ep for ep in b[3].values() if ep.air_date > datetime.now()])
 
+# search('bobs burgers')
+
+seinfeld = Show(s)
+broad = Show(b)
